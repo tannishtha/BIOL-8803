d619589e8b0bde968b0e9bebf9317ef208c9ad75
Tannishtha Som <tannishtha.som@gmail.com>
2016-11-22 17:05:19 -0500

    Add files via upload

---
+++ ex6/wrapper6.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+./week6.sh -n 10 -m 8 -v
+
+###############################################
+##
+## Your code should run with, BUT NOT LIMITED TO, the command(s) above.
+## Please read the exercise instructions for more details on how your code should work.
+##
+###############################################
